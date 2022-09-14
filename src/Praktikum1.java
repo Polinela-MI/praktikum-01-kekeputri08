3b300a253190da8ee87c08552027149705192a06
kekeputri08 <113098935+kekeputri08@users.noreply.github.com>
2022-09-14 19:40:50 +0700

    Create Praktikum1.java
    mikir dulu bentar1(eror di akhir ;)

---
+++ src/Praktikum1.java
@@ -0,0 +1,10 @@
+public class Praktikum1 {
+
+    public static void main(String[] args) {
+
+        char operator = args[0].charAt(0);
+        int bil1 = Integer.parseInt(args[1]);
+        int bil2 = Integer.parseInt(args[2]);
+
+        System.out.print(args[0]+ " " +args[1]+ " " +args[2]+ " = ");
+        
