4bfb9fb740b0f62b6906031974f9c1fd13c253db
kekeputri08 <113098935+kekeputri08@users.noreply.github.com>
2022-09-21 21:38:12 +0700

    Create Praktikum2.java
    percobaan1

---
+++ src/Praktikum2.java
@@ -0,0 +1,20 @@
+package Modul1;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
+    public class Praktikum2 {
+        public static void main(String[] args) {
+
+            for (int i = 1; i < args.length; i += 3) {
+                int NIK = Integer.parseInt(args[i]);
+                String Nama = args[i + 1];
+                int Gajipokok = Integer.parseInt(args[i + 2]);
+
+                int Trans, Kerja, Total, belum_pajak, sudah_pajak;
+
+
+            }
+        }
+    }
+
+
