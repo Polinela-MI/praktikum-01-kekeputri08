be66a30292869052140bf08fa28751785494942b
kekeputri08 <113098935+kekeputri08@users.noreply.github.com>
2022-09-28 19:38:16 +0700

    Create Praktikum3.java
    class

---
+++ src/modul3/Praktikum3.java
@@ -0,0 +1,25 @@
+package modul3;
+
+public class Praktikum3 {
+    int nik;
+    String nama;
+    int gapok;
+
+    //overloading constructor
+    public Praktikum3(int nik, String nama, int gapok){
+        this.nik=nik;
+        this.nama=nama;
+        this.gapok=gapok;
+    }
+
+    public int hitungGaji(){
+
+        int Trans = (int)(gapok * 0.05);
+        int Kerja = (int)(gapok * 0.1);
+
+        int Sebelum_Pajak = gapok + Kerja + Trans;
+        int Sesudah_Pajak = (int) (Sebelum_Pajak * 0.025);
+        int Total = Sebelum_Pajak - Sesudah_Pajak;
+        return Total;
+    }
+}
