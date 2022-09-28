cc2997ce49b4e956cafc6ba9eff29a7a18221202
kekeputri08 <113098935+kekeputri08@users.noreply.github.com>
2022-09-28 22:57:28 +0700

    Update Praktikum3.java
    banyak banget erornya

--- src/modul3/Praktikum3.java
+++ src/modul3/Praktikum3.java
@@ -4,10 +4,10 @@ public class Praktikum3 {
     int nik;
     String nama;
     int gapok;
-    int kehadiran;
+    int hadir;
 
     //overloading constructor
-    public Praktikum3(int nik, String nama, int gapok, int kehadiran){
+    public Praktikum3(int nik, String nama, int gapok, int hadir){
         this.nik=nik;
         this.nama=nama;
         this.gapok=gapok;
@@ -22,5 +22,25 @@ public class Praktikum3 {
         int Sesudah_Pajak = (int) (Sebelum_Pajak * 0.025);
         int Total = Sebelum_Pajak - Sesudah_Pajak;
         return Total;
+    }
+}
+
+public class main{
+    public static void main(String[] args){
+        ArrayList<Integer> nikList = new ArrayList<Integer>();
+
+        for(int i=1; i<args.length; i+=3){
+            int nik =args[i];
+            String nama = args[i+1];
+            int gaji =args[i+2];
+
+            if(nikList.contains(nik)){
+                Praktikum3 p = new Praktikum3(nik, nama, gaji, hadir);
+                nikList.add(nik);
+                System.out.println()
+
+            }
+
+        }
     }
 }
