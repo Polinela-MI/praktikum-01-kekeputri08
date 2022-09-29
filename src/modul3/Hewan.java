e16daae7c994bc051f079ddfde436a3721e4ae6a
kekeputri08 <113098935+kekeputri08@users.noreply.github.com>
2022-09-29 15:08:49 +0700

    Create Hewan.java
    praktiiikk p4

---
+++ src/modul3/Hewan.java
@@ -0,0 +1,35 @@
+package modul3;
+
+class Hewan{
+    int jumlahKaki;
+
+    Hewan() {
+        this.jumlahKaki = 0;
+        System.out.println("Saya adalah Hewan tak berkaki.");
+    }
+
+
+    Hewan(int jumlahKaki) {
+        this.jumlahKaki = jumlahKaki;
+        System.out.print("Saya adalah Hewan");
+        System.out.print("jumlah kaki saya:" + this.jumlahKaki);
+    }
+}
+     class Ayam extends Hewan{
+        Ayam(){
+            System.out.println("Tapi Saya adalah Ayam");
+        }
+
+        Ayam(int jumlahKaki){
+            System.out.println("Saya Ayam juga lho");
+        }
+    }
+
+
+class InheritanceHewanMain{
+    public static void main(String[] args){
+       Ayam kaefsi = new Ayam();
+           System.out.println("=======");
+           Ayam kalkun = new Ayam(2);
+       }
+    }
