de524c415f02b5f1736613c37aae6f062a929929
kekeputri08 <113098935+kekeputri08@users.noreply.github.com>
2022-09-29 15:08:29 +0700

    Create latihan1.java
    inheritance

---
+++ src/modul3/latihan1.java
@@ -0,0 +1,32 @@
+package modul3;
+
+public class latihan1 {
+    String nama;
+
+    void sapa(){
+        System.out.println("Halo, saya" + nama );
+    }
+}
+class Student extends latihan1 {
+    String npm;
+    String major;
+
+    @Override
+    void sapa() {
+        super.sapa();
+        System.out.println("Saya Mahasiswa" + major);
+    }
+}
+class main{
+    public static void main(String[] args){
+        latihan1 joko = new latihan1();
+        joko.nama = "Joko";
+        joko.sapa();
+
+        Student dodo = new Student();
+        dodo.nama = "Widodo";
+        dodo.npm = "221565";
+        dodo.major = "ilmu komputer";
+        dodo.sapa();
+    }
+}
