a36c38e5f6e2772ccab03ff6537c694534d17a65
kekeputri08 <113098935+kekeputri08@users.noreply.github.com>
2022-09-29 15:41:56 +0700

    Update Karyawan.java
    2

--- src/modul4/Karyawan.java
+++ src/modul4/Karyawan.java
@@ -5,7 +5,23 @@ public class Karyawan {
     private String nama;
     private int gajiPokok;
 
-    
+    Karyawan(int nik, String nama, int gajiPokok){
+        this.nik = nik;
+        this.nama = nama;
+        this.gajiPokok = gajiPokok;
+        System.out.println("Kelas Karyawan");
+    }
 }
 
+class ParuhWaktu{
+    int jamKerja = 20;
+}
+
+class PenuhWaktu{
+    int jamKerja = 40;
+}
+
+class Manajer{
+    int Kinerja = ;
+}
 
