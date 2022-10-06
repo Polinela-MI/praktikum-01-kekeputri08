5e4aba14485ebfdc12be50931fe443bda2f06889
kekeputri08 <113098935+kekeputri08@users.noreply.github.com>
2022-10-06 14:48:13 +0700

    Create Hewan.java
    Polimorfisme

---
+++ src/modul5/Hewan.java
@@ -0,0 +1,29 @@
+package modul5;
+
+public class Hewan {
+}
+class Ayam extends Hewan{
+}
+class Kucing extends Hewan{
+}
+class Iguana extends Hewan {
+}
+
+
+class Piaraan{
+    static void main(String[] args){
+        Hewan makhluk = new Hewan();
+        Hewan polo = new Ayam();
+        Hewan cimoy = new Kucing();
+        Hewan reptil = new Iguana();
+
+//Casting ke tipe yang sebenarnya
+        Ayam newPolo = (Ayam) polo;
+        Kucing momoy = (Kucing) cimoy;
+        Iguana blizz = (Iguana) reptil;
+
+//Contoh salah:
+        Iguana snowy = (Iguana) polo;
+    }
+}
+
