8588e1d803c238303cf85e85f40cd3a5e8ba3fa5
kekeputri08 <113098935+kekeputri08@users.noreply.github.com>
2022-10-06 19:00:33 +0700

    Update PolimorfismeMain.java
    soal 2

--- src/modul5/PolimorfismeMain.java
+++ src/modul5/PolimorfismeMain.java
@@ -1,26 +1,71 @@
 package modul5;
 
 public class PolimorfismeMain {
-    public static void cetakSlipGaji(Karyawan karyawan{
+    public static void cetakSlipGaji(Karyawan karyawan) {
 
         double Total = karyawan.hitungGaji();
         double subtotal = Total - karyawan.getPajak();
 
-        System.out.println(karyawan.getNik() + " - " + karyawan.getNama());
-        System.out.println("Total jam kerja : " + );
 
-        System.out.println("---");
+        if (karyawan instanceof KaryawanParuhWaktu paruh) {
 
-        System.out.println("Gaji Pokok : "+ karyawan.getgajiPokok());
-        System.out.println("Tunjangan Transportasi : " + karyawan.getTunjanganTranportasi());
-        System.out.println("Bonus : " + karyawan.getMisc());
-        System.out.println("Pajak :" + karyawan.getPajak());
-        System.out.println("Gaji Sebelum Pajak : " + subtotal);
+            paruh.getJamKerja();
 
-        System.out.println("----");
+            System.out.println(karyawan.getNik() + " - " + karyawan.getNama());
+            System.out.println("Total jam kerja : " + KaryawanParuhWaktu.getJamKerja());
 
-        System.out.println("Gaji yang Diterima :" + Total);
+            System.out.println("_________________________________________");
+
+            System.out.println("Gaji Pokok : " + karyawan.getgajiPokok());
+            System.out.println("Tunjangan Transportasi : " + karyawan.getTunjanganTranportasi());
+            System.out.println("Bonus : " + karyawan.getMisc());
+            System.out.println("Pajak : " + karyawan.getPajak());
+            System.out.println("Gaji Sebelum Pajak : " + subtotal);
+
+            System.out.println("_________________________________________");
+
+            System.out.println("Gaji yang diterima : " + Total);
 
 
+        } else if (karyawan instanceof KaryawanPenuhWaktu penuh) {
+
+            penuh.getJamKerja();
+
+            System.out.println(karyawan.getNik() + " - " + karyawan.getNama());
+            System.out.println("Total jam kerja : " + KaryawanPenuhWaktu.getJamKerja());
+
+            System.out.println("__________________________________________");
+
+            System.out.println("Gaji Pokok : " + karyawan.getgajiPokok());
+            System.out.println("Tunjangan Transportasi : " + karyawan.getTunjanganTranportasi());
+            System.out.println("Bonus : " + karyawan.getMisc());
+            System.out.println("Pajak : " + karyawan.getPajak());
+            System.out.println("Gaji Sebelum Pajak : " + subtotal);
+
+            System.out.println("_________________________");
+
+            System.out.println("Gaji yang diterima : " + Total);
+
+        } else if (karyawan instanceof Manajer manajer) {
+
+            manajer.getKinerja();
+
+            System.out.println(karyawan.getNik() + " - " + karyawan.getNama());
+            System.out.println("Total kinerja : "+ manajer.getKinerja());
+
+            System.out.println("________________________________________");
+
+            System.out.println("Gaji Pokok : " + karyawan.getgajiPokok());
+            System.out.println("Tunjangan Transportasi : " + karyawan.getTunjanganTranportasi());
+            System.out.println("Bonus : " + karyawan.getMisc());
+            System.out.println("Pajak : " + karyawan.getPajak());
+            System.out.println("Gaji Sebelum Pajak : " + subtotal);
+
+            System.out.println("________________________________________");
+
+            System.out.println("Gaji yang diterima : " + Total);
+
+
+        }
     }
 }
