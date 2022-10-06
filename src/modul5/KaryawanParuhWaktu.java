a9f94f7c27acb26b3138aacc99e90eb16ddb0468
kekeputri08 <113098935+kekeputri08@users.noreply.github.com>
2022-10-06 16:28:00 +0700

    Update KaryawanParuhWaktu.java
    soal1 a

--- src/modul5/KaryawanParuhWaktu.java
+++ src/modul5/KaryawanParuhWaktu.java
@@ -13,16 +13,16 @@ public class KaryawanParuhWaktu extends Karyawan {
         double gajiMingguan;
         gajiMingguan= getgajiPokok()*20;
         tunjanganTransportasi=gajiMingguan*0.5;
-        misc=gajiMingguan+tunjanganTransportasi;
+        double subtotal=gajiMingguan+tunjanganTransportasi;
         if(jamKerja>20){
             double lembur =(jamKerja-20)*(getgajiPokok()+2000);
-            misc=misc+lembur;
+            subtotal=subtotal+lembur;
         }else if(jamKerja<20){
-            double penalti=(20-jamKerja)*getgajiPokok();
-            misc=misc-penalti;
+            double misc=(20-jamKerja)*getgajiPokok();
+            subtotal=subtotal-misc;
         }
-        pajak = misc+0.025;
-        double total = misc-pajak;
+        pajak = subtotal+0.025;
+        double total = subtotal-pajak;
         return total;
     }
 
